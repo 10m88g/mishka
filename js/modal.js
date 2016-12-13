990f05b64426a0a25e28f96de5d0ca1bbdfb42ba
Anton Cherepov <jacobxblue@gmail.com>
2016-12-13 21:29:01 +0300

    js логика модального окна

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+function hideModal() {
+  var modal = document.getElementById("modal-window");
+  modal.classList.add("modal--hidden");
+}
+
+function viewModal() {
+  var modal = document.getElementById("modal-window");
+  modal.classList.remove("modal--hidden");
+}
